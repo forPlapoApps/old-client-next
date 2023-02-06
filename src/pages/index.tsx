c4f2f2d34259d4011e618a93f85c7c0d60492baf
hulk-higakijin <hm385.chejptks@gmail.com>
2023-02-06 13:31:18 +0900

    chore: add flow-bite

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
+import { Button } from 'flowbite-react'
+
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+
+      <Button>hogehoge</Button>
+
   )
 }
