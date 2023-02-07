74440aea387ac9a4ca0f23d39e9a4672b5e9cb64
hulk-higakijin <hm385.chejptks@gmail.com>
2023-02-07 12:03:46 +0900

    feat: make RoomsTable #2

--- src/pages/rooms.tsx
+++ src/pages/rooms.tsx
@@ -1,11 +1,21 @@
+import Layout from "@/core/layouts/Layout";
+import RoomsTable from "@/rooms/components/RoomsTable";
 import { NextPage } from "next";
 
 const RoomsPage: NextPage = () => {
   return (
-    <div>
-      <p>RoomsPage</p>
-    </div>
-  )
-}
+    <Layout>
+      <h2 className="text-2xl font-bold">Select your Room!</h2>
+      <div className="grid grid-cols-4 gap-4">
+        <div className="col-span-3">
+          <RoomsTable />
+        </div>
+        <div className="col-span-1">
+          welcome to forPlapoApps!
+        </div>
+      </div>
+    </Layout>
+  );
+};
 
-export default RoomsPage
+export default RoomsPage;
