084109a5fa4146349dd2530a28b5df48dd6b4d83
hulk-higakijin <hm385.chejptks@gmail.com>
2023-02-08 11:12:11 +0900

    Be dynamic in RoomsComponent #2

--- src/rooms/components/RoomsTable.tsx
+++ src/rooms/components/RoomsTable.tsx
@@ -1,70 +1,36 @@
 import { Table } from "flowbite-react";
+import Link from "next/link";
 
-type InitialProps = { 
-  rooms: Room[]
-}
+type InitialProps = {
+  rooms: Room[];
+};
 
 const RoomsTable = ({ rooms }: InitialProps) => {
   return (
     <Table>
       <Table.Head>
-        <Table.HeadCell>Product name</Table.HeadCell>
-        <Table.HeadCell>Color</Table.HeadCell>
-        <Table.HeadCell>Category</Table.HeadCell>
-        <Table.HeadCell>Price</Table.HeadCell>
+        <Table.HeadCell>Name</Table.HeadCell>
+        <Table.HeadCell>Members</Table.HeadCell>
+        <Table.HeadCell>Created At</Table.HeadCell>
         <Table.HeadCell>
           <span className="sr-only">Edit</span>
         </Table.HeadCell>
       </Table.Head>
       <Table.Body className="divide-y">
-        <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-          <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-            Apple MacBook Pro 17"
-          </Table.Cell>
-          <Table.Cell>Sliver</Table.Cell>
-          <Table.Cell>Laptop</Table.Cell>
-          <Table.Cell>$2999</Table.Cell>
-          <Table.Cell>
-            <a
-              href="/tables"
-              className="font-medium text-blue-600 hover:underline dark:text-blue-500"
-            >
-              Edit
-            </a>
-          </Table.Cell>
-        </Table.Row>
-        <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-          <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-            Microsoft Surface Pro
-          </Table.Cell>
-          <Table.Cell>White</Table.Cell>
-          <Table.Cell>Laptop PC</Table.Cell>
-          <Table.Cell>$1999</Table.Cell>
-          <Table.Cell>
-            <a
-              href="/tables"
-              className="font-medium text-blue-600 hover:underline dark:text-blue-500"
-            >
-              Edit
-            </a>
-          </Table.Cell>
-        </Table.Row>
-        <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-          <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-            Magic Mouse 2
-          </Table.Cell>
-          <Table.Cell>Black</Table.Cell>
-          <Table.Cell>Accessories</Table.Cell>
-          <Table.Cell>$99</Table.Cell>
-          <Table.Cell>
-            <a
-              href="/tables"
-              className="font-medium text-blue-600 hover:underline dark:text-blue-500"
-            >
-              Edit
-            </a>
-          </Table.Cell>
-        </Table.Row>
+        {rooms.map((room) => (
+          <Table.Row key={room.id} className="bg-white dark:border-gray-700 dark:bg-gray-800">
+            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+              {room.name}
+            </Table.Cell>
+            <Table.Cell>{room.participantsLength}</Table.Cell>
+            <Table.Cell>{room.createdAt}</Table.Cell>
+            <Table.Cell>
+              <Link href={`/rooms/${room.id}`} className="title">
+                Visit
+              </Link>
+            </Table.Cell>
+          </Table.Row>
+        ))}
       </Table.Body>
     </Table>
   );
