bb6890a3de8cbdadb21ed530a7569a33dfda8b53
hulk-higakijin <hm385.chejptks@gmail.com>
2023-02-08 12:07:09 +0900

    feat: add room detail page #3

---
+++ src/pages/rooms/[roomId].tsx
@@ -0,0 +1,11 @@
+import Layout from "@/core/layouts/Layout"
+
+const ShowRoomPage = () => {
+  return (
+    <Layout title="">
+      <p>RoomDetailPage</p>
+    </Layout>
+  )
+}
+
+export default ShowRoomPage
