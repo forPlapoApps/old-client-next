1837cdb88ce205a67cfeb1cd63d80132c8f37d70
hulk-higakijin <hm385.chejptks@gmail.com>
2023-02-08 12:15:24 +0900

    feat: Be dynamic in room deail page #3

--- src/pages/rooms/[roomId].tsx
+++ src/pages/rooms/[roomId].tsx
@@ -1,11 +1,39 @@
-import Layout from "@/core/layouts/Layout"
+import Layout from "@/core/layouts/Layout";
+import { GetServerSideProps, NextPage } from "next";
+import fetchData from "util/fetchData";
 
-const ShowRoomPage = () => {
+type InitialProps = {
+  room: Room;
+};
+
+const ShowRoomPage: NextPage<InitialProps> = ({ room }) => {
   return (
     <Layout title="">
-      <p>RoomDetailPage</p>
+      <div>
+        <p>RoomDetailPage</p>
+        <p>{room.name}</p>
+      </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default ShowRoomPage
+export const getServerSideProps: GetServerSideProps = async ({
+  res,
+  query,
+}) => {
+  const room = await fetchData(`/rooms/${query.roomId}`, res);
+
+  if (room.statusCode == 404) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      room,
+    },
+  };
+};
+
+export default ShowRoomPage;
