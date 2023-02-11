239edba05aefc40d9133d67fcc57338db64ccba0
hulk-higakijin <hm385.chejptks@gmail.com>
2023-02-11 21:30:14 +0900

    style: 505 page with ErrorComponent #14

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -1,11 +1,16 @@
+import ErrorComponent from "@/core/components/Error";
 import { NextPage } from "next";
 
 const InternalServerErrorPage: NextPage = () => {
   return (
-    <div>
-      custom error page
-    </div>
-  )
-}
+    <ErrorComponent
+      title="Server Error"
+      statusCode={500}
+      headline="Something bad just happened..."
+      description="Our servers could not handle your request. Try refreshing the page."
 
-export default InternalServerErrorPage
+    />
+  );
+};
+
+export default InternalServerErrorPage;
