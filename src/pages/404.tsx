92832cc6672c84a162dbcccdf8fc754261e8cea2
hulk-higakijin <hm385.chejptks@gmail.com>
2023-02-11 21:07:58 +0900

    style: 404 page #14

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,79 @@
+import Layout from "@/core/layouts/Layout";
+import {
+  createStyles,
+  Title,
+  Text,
+  Button,
+  Container,
+  Group,
+} from "@mantine/core";
 import { NextPage } from "next";
+import { useRouter } from "next/router";
+
+const useStyles = createStyles((theme) => ({
+  root: {
+    paddingTop: 80,
+    paddingBottom: 80,
+  },
+
+  label: {
+    textAlign: "center",
+    fontWeight: 900,
+    fontSize: 220,
+    lineHeight: 1,
+    marginBottom: theme.spacing.xl * 1.5,
+    color: theme.colors.dark[4],
+
+    [theme.fn.smallerThan("sm")]: {
+      fontSize: 120,
+    },
+  },
+
+  title: {
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    textAlign: "center",
+    fontWeight: 900,
+    fontSize: 38,
+
+    [theme.fn.smallerThan("sm")]: {
+      fontSize: 32,
+    },
+  },
+
+  description: {
+    maxWidth: 500,
+    margin: "auto",
+    marginTop: theme.spacing.xl,
+    marginBottom: theme.spacing.xl * 1.5,
+  },
+}));
 
 const NotFoundPage: NextPage = () => {
-  return (
-    <div>
-      <p>not found</p>
-    </div>
-  )
-}
+  const { classes } = useStyles();
+  const router = useRouter();
 
-export default NotFoundPage
+  return (
+    <Layout title="Not Found !">
+      <Container className={classes.root}>
+        <div className={classes.label}>404</div>
+        <Title className={classes.title}>You have found a secret place.</Title>
+        <Text
+          color="dimmed"
+          size="lg"
+          align="center"
+          className={classes.description}
+        >
+          Unfortunately, this is a 404 page. You may have mistyped the
+          address, or the page has been moved to another URL.
+        </Text>
+        <Group position="center">
+          <Button onClick={() => {router.push('/')}} variant="subtle" size="md">
+            Take me back to home page
+          </Button>
+        </Group>
+      </Container>
+    </Layout>
+  );
+};
+
+export default NotFoundPage;
